20eb819f94a3e8d3427695d0ae5a40c21e988432
NacheteTnf <ignaciocabrerahdez97@gmail.com>
2023-04-04 14:02:00 +0100

    first commit

---
+++ client/src/canva/index.jsx
@@ -0,0 +1,29 @@
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
+
+import Backdrop from "./Backdrop";
+import CameraRing from "./CameraRing";
+import Shirt from "./Shirt";
+
+const CanvasModel = () => {
+  return (
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+
+      <CameraRing>
+        <Backdrop />
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRing>
+    </Canvas>
+  );
+};
+
+export default CanvasModel;
